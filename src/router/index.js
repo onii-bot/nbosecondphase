60d6546595c249a770b3a2d11c4cbdff914e99da
sukunazz <124653324+sukunazz@users.noreply.github.com>
2024-07-25 12:58:31 +0545

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   mode:'history',
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
